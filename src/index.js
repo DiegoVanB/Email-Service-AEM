7164ff06a4c549cce40a8b69a5cf4ba80eeb1844
blldgg <blldgg@forem.be>
2024-01-30 10:00:37 +0100

    Pushing to repo

--- src/index.js
+++ src/index.js
@@ -3,22 +3,8 @@ import cors from 'cors';
 import OpenAI from "openai";
 import 'dotenv/config';
 import config from '../config.js';
-import functions from 'firebase-functions';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { sendContactForm } from "./controllers/forms/forms.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyDZ5oN9Z1gXa3H_a1iVXrWzJgzBAe_qaL8",
-  authDomain: "email-server-aem.firebaseapp.com",
-  projectId: "email-server-aem",
-  storageBucket: "email-server-aem.appspot.com",
-  messagingSenderId: "1012220487058",
-  appId: "1:1012220487058:web:1e30c7eebce14fe503cd4b",
-  measurementId: "G-MQJ1EHSX92"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const openai = new OpenAI({apiKey: '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'});
 const server = express()
 
@@ -51,5 +37,3 @@ server.get('/get-comment', async (req, res) => {
 server.listen(config.port, () => {
     console.log(`Server is running on port ${config.hostUrl}`);
 })
-
-exports.api = functions.https.onRequest(server);
